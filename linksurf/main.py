4ee5b9a0c4e218ec7d53932674e4af34de8f1ef0
Victor Guedes <victorfelipe.guedes@hotmail.com>
2025-12-24 01:39:17 -0300

    feat: track crawled urls and its depth

--- linksurf/main.py
+++ linksurf/main.py
@@ -4,6 +4,20 @@ from typing import List
 from uuid import uuid4
 
 
+class URL:
+    def __init__(self, address: str, depth: int = 1):
+        self.address = address
+        self.depth = depth
+        self.status = URLStatus.PENDING
+
+
+class URLStatus(Enum):
+    PENDING = 1
+    CRAWLED = 2
+    FAILED = 3
+    SKIPPED = 4
+
+
 class Page:
     def __init__(self, url: str, title: str, description: str):
         self.id = uuid4()
@@ -11,24 +25,22 @@ class Page:
         self.title = title
         self.description = description
         self.noindex = False
+        self.links: List[Link] = []
 
     def parse(self):
         print(f"Parsing page {self.url}")
 
-        if self.url == "https://quotes.toscrape.com":
-            hyperlinks = ["https://books.toscrape.com"]
+        found_links = [
+            Link(source=self.url, target="https://books.toscrape.com", type=LinkType.EXTERNAL,
+                 text="Anchor text goes here", nofollow=False)
+        ]
 
-            print(f"Found {len(hyperlinks)} hyperlinks")
+        print(f"Found {len(found_links)} hyperlinks")
 
-            for hyperlink in hyperlinks:
-                text = "Anchor text goes here"
-                nofollow = False  # TODO: Check for link's nofollow directive
-                type = LinkType.EXTERNAL  # Only exact hostname should count as internal
-                link = Link(source=url, target=hyperlink, type=type, text=text, nofollow=nofollow)
+        self.links.extend(found_links)
+        links.extend(found_links)
 
-                links.append(link)
-
-                crawl_page(hyperlink)
+        print(f"Finished parsing page {self.url}")
 
 
 class Link:
@@ -46,13 +58,21 @@ class LinkType(Enum):
     EXTERNAL = 2
 
 
-urls: List[str] = []
+urls: List[URL] = []
 pages: List[Page] = []
 links: List[Link] = []
 
 
-def can_crawl_page(url: str) -> bool:
-    domain = url.split("/")[0]
+def is_page_already_crawled(address: str) -> bool:
+    for url in urls:
+        if url.address == address:
+            return True
+
+    return False
+
+
+def can_crawl_page(address: str) -> bool:
+    domain = address.split("/")[0]
     robots_url = f"{domain}/robots.txt"
 
     # TODO: Check robots meta tag
@@ -60,28 +80,56 @@ def can_crawl_page(url: str) -> bool:
     return random.choice([True, False])
 
 
-def crawl_page(url: str):
-    if not can_crawl_page(url):
-        print(f"Skipping {url}")
+def crawl(address: str, depth: int = 1, max_depth: int = 4):
+    url = URL(address=address, depth=depth)
+
+    if is_page_already_crawled(url.address):
+        print(f"Skipping {url.address}: already crawled")
+
         return
 
-    print(f"Crawling {url}")
+    if not can_crawl_page(url.address):
+        print(f"Skipping {url.address}: not allowed")
+
+        return
+
+    if depth > max_depth:
+        print(f"Skipping {url.address}: reached max depth")
+
+        url.status = URLStatus.SKIPPED
+
+    # TODO: Check if url was skipped before, change depth if lower and crawl
+
+    urls.append(url)
+
+    if url.status == URLStatus.SKIPPED:
+        return
+
+    print(f"Crawling {url.address}")
 
     title = "Title goes here"
     description = "Description goes here"
-    page = Page(url=url, title=title, description=description)
+    page = Page(url=url.address, title=title, description=description)
 
     pages.append(page)
 
     page.parse()
 
+    for link in page.links:
+        crawl(link.target, depth + 1)
+
+    url.status = URLStatus.CRAWLED
+
 
 if __name__ == '__main__':
-    starting_url = "https://quotes.toscrape.com"
+    seed = ["https://quotes.toscrape.com"]
 
-    urls.append(starting_url)
+    print(f"Starting crawl with {len(seed)} urls\n")
 
-    print(f"Starting crawl with {len(urls)} urls\n")
+    for url in seed:
+        crawl(url)
 
-    for url in urls:
-        crawl_page(url)
+    print(f"\nFinished crawl with {len(urls)} urls")
+
+    for index, url in enumerate(urls):
+        print(f"[{index}]: {url.address=} {url.status=} {url.depth=}")
