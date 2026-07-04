b86c04f55e939c99f6716ca69c060cc6da247902
Victor Guedes <victorfelipe.guedes@hotmail.com>
2026-07-04 17:24:26 -0300

    chore: get services credentials and proxy from .env

--- linksurf/main.py
+++ linksurf/main.py
@@ -1,40 +1,52 @@
+from dotenv import load_dotenv
+
 from linksurf.application import Linksurf
 from linksurf.broker.rabbitmq import RabbitMQBroker
 from linksurf.common.models import URL
 from linksurf.common.settings import Settings
-
 from linksurf.services import Services
 from linksurf.services.blob import S3BlobStorage
 from linksurf.services.cache import RedisCache
 from linksurf.services.database import MongoDatabase
 from linksurf.services.fetcher import RequestsFetcher
+from linksurf.utils.env import get_env
+
+load_dotenv()
 
 if __name__ == "__main__":
     seed = [
-        URL("http://example.com"),
-        URL("https://example.com"),
-        URL("https://example.com/abc"),
-        URL("https://example.com/def"),
-        URL("https://example.com/ghi"),
-        URL("http://example.com:80"),
-        URL("https://example.com:443"),
-        URL("https://example.com:42069"),
-        URL("ftp://username:password@example.com"),
-        # URL("https://www.goodreads.com/"),
-        # URL("https://www.reddit.com/"),
+        # Base cases for Rule/Filter testing
+        # URL("http://example.com"),
+        # URL("https://example.com"),
+        # URL("https://example.com/abc"),
+        # URL("https://example.com/def"),
+        # URL("https://example.com/ghi"),
+        # URL("http://example.com:80"),
+        # URL("https://example.com:443"),
+        # URL("https://example.com:42069"),
+        # URL("ftp://username:password@example.com"),
+
+        # Safe and with 100s of pages
+        URL("https://quotes.toscrape.com/"),
+
+        # Not allowed
+        URL("https://www.reddit.com/"),
+
+        # HTML's <base> tag testing
+        URL("https://base-tag-test.vercel.app/"),
     ]
 
     services = Services(
-        database=MongoDatabase(url="mongodb://root:root@localhost:27017"),
+        database=MongoDatabase(url=get_env("MONGODB_URL")),
         blob_storage=S3BlobStorage(
-            bucket="linksurf",
-            endpoint_url="http://localhost:9000",
-            access_key="minioadmin",
-            secret_key="minioadmin",
+            bucket=get_env("S3_BUCKET"),
+            endpoint_url=get_env("S3_ENDPOINT_URL"),
+            access_key=get_env("S3_ACCESS_KEY"),
+            secret_key=get_env("S3_SECRET_KEY"),
         ),
         cache=RedisCache(
-            host="localhost",
-            port=6379,
+            host=get_env("REDIS_HOST"),
+            port=get_env("REDIS_PORT", cast=int),
         ),
         fetcher=RequestsFetcher(),
     )
@@ -44,6 +56,7 @@ if __name__ == "__main__":
     settings = Settings(
         identifier="Linksurf",
         user_agent="Linksurf/1.0",
+        proxy=get_env("PROXY_URL", required=False),
     )
 
     linksurf = Linksurf(settings=settings, services=services, broker=broker)
