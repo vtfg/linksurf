19a6e90894d089efbc994c38fa844854aae79d5e
Victor Guedes <victorfelipe.guedes@hotmail.com>
2026-07-29 18:38:42 -0300

    feat: track content languages and block domain if mismatches

--- linksurf/components/parser/filters.py
+++ linksurf/components/parser/filters.py
@@ -1,14 +1,28 @@
 from linksurf.common.models import Language
 from linksurf.common.payload import Payload
+from linksurf.common.settings import Settings
+from linksurf.common.types import Error
 from linksurf.components.base import Filter, FilterResponse
 from linksurf.components.parser.middlewares import LanguageMiddleware
+from linksurf.services import Database, Services
 
 
 class LanguageFilter(Filter):
+    """
+    Basic language filter that uses the code returned from the LanguageMiddleware.
+
+    Records the language metric to the domain's Database record.
+    """
+
     DEPENDS_ON = [LanguageMiddleware]
+
+    database: Database
 
     def __init__(self, allowed: list[Language]):
         self.allowed = allowed
+
+    async def on_start(self, settings: Settings, services: Services):
+        self.database = services.database
 
     async def execute(self, payload: Payload) -> FilterResponse:
         code = payload.get_metadata("language")
@@ -17,5 +31,12 @@ class LanguageFilter(Filter):
             language = Language(code)
         except ValueError:
             language = None
+
+        if language is not None:
+            try:
+                await self.database.record_language(payload.url.domain, language.value,
+                                                    allowed=language in self.allowed)
+            except Exception as e:
+                return FilterResponse(False, Error("Failed to record language metric.", retriable=True, exception=e))
 
         return FilterResponse(language in self.allowed, None)
